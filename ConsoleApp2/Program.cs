c983151b3efd84e096c89441febebc3fd2e82ef0
vahra <vahra@DESKTOP-SO0L1VA>
2022-07-07 23:15:58 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,117 @@
+﻿// See https://aka.ms/new-console-template for more information
+//xndir 31
+
+//int a, b, c, d;
+
+
+//a=Convert.ToInt32(Console.ReadLine());
+//b=Convert.ToInt32(Console.ReadLine());
+//c=Convert.ToInt32(Console.ReadLine());
+//d=Convert.ToInt32(Console.ReadLine());
+
+
+//if (a > b && a > c && a > d)
+//{
+//    Console.WriteLine(a);
+//}
+//else if (b > c && b > d && b > a)
+//{
+//    Console.WriteLine(b);
+//}
+//else if (c > b && c > d && c > a)
+//{
+//    Console.WriteLine(c);
+//}
+//else Console.WriteLine(d);
+
+
+
+//xndir 32
+
+//int a, b, c, d;
+
+//a = Convert.ToInt32(Console.ReadLine());
+//b = Convert.ToInt32(Console.ReadLine());
+//c = Convert.ToInt32(Console.ReadLine());
+//d = Convert.ToInt32(Console.ReadLine());
+
+//if (a < b && a < c && a < d)
+//{
+//    Console.WriteLine(a);
+//}
+//else if (b < c && b < d && b < a)
+//{
+//    Console.WriteLine(b);
+//}
+//else if (c < b && c < d && c < a)
+//{
+//    Console.WriteLine(c);
+//}
+//else Console.WriteLine(d);
+
+
+//xndir 33
+
+
+//int a, b, c, d;
+
+//a = Convert.ToInt32(Console.ReadLine());
+//b = Convert.ToInt32(Console.ReadLine());
+//c = Convert.ToInt32(Console.ReadLine());
+//d = Convert.ToInt32(Console.ReadLine());
+
+//if (a == 1 || b == 1 || c == 1 || d == 1)
+//{
+//    Console.WriteLine(true);
+//}
+//else Console.WriteLine(false);
+
+
+
+//xndir 34
+
+//int a, b, c, d;
+
+//a = Convert.ToInt32(Console.ReadLine());
+//b = Convert.ToInt32(Console.ReadLine());
+//c = Convert.ToInt32(Console.ReadLine());
+//d = Convert.ToInt32(Console.ReadLine());
+
+//if (a + b == c + d || a + c == b + d || a + d == b + c)
+//{
+//    Console.WriteLine(true);
+//}
+//else Console.WriteLine(false);
+
+//xndir 35
+
+
+//int a, b, c, d;
+
+//a = Convert.ToInt32(Console.ReadLine());
+//b = Convert.ToInt32(Console.ReadLine());
+//c = Convert.ToInt32(Console.ReadLine());
+//d = Convert.ToInt32(Console.ReadLine());
+
+//if (a == b + c + d || b == a + c + d || c == a + b + d || d == a + b + c)
+//{
+//    Console.WriteLine(true);
+//}
+//else Console.WriteLine(false);
+
+
+//xndir 36
+
+
+//int a, b, c, d;
+
+//a = Convert.ToInt32(Console.ReadLine());
+//b = Convert.ToInt32(Console.ReadLine());
+//c = Convert.ToInt32(Console.ReadLine());
+//d = Convert.ToInt32(Console.ReadLine());
+//if (a % 2 == 1 && b % 2 == 1 || a % 2 == 1 && c % 2 == 1 || a % 2 == 1 && d % 2 == 1 || b % 2 == 1 && c % 2 == 1 || b % 2 == 1 && d % 2 == 1 || c % 2 == 1 && d % 2 == 1)
+//{
+//    Console.WriteLine(true);
+//}
+//else Console.WriteLine(false);
+
